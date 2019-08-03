6c4b3a22288cd04e3e0f72b1c47a67295878fadf
Glitch (brainy-polyester) <none>
2019-08-03 10:41:24 +0000

    🎹👆 Checkpoint ./server.js:577339/4674 ./package.json:577339/1271

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
+const fs = require('fs');
+const axios = require('axios');
 
 const config = {
   channelAccessToken: process.env.TOKEN,
@@ -17,18 +19,62 @@ app.post('/webhooks/line', line.middleware(config), (req, res) => {
     });
 });
 
-app.use(express.static('static'));
+app.use(express.static('static'))
+
+const dataPath = key => `.data/${key}.json`
+const storage = {
+  async get(key) {
+    const filePath = dataPath(key)
+    if (fs.existsSync(filePath)) {
+      return fs.readFileSync(filePath, 'utf8')
+    } else {
+      return null
+    }
+  },
+  async set(key, value) {
+    const filePath = dataPath(key)
+    return fs.writeFileSync(filePath, String(value), 'utf8')
+  },
+}
 
 const client = new line.Client(config);
-const handleEvent = (event) => {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
+const handleEvent = async event => {
+  if (event.type !== 'message') {
+    return null;
+  }
+  const userId = event.source.userId
+  if (!userId || !/^U\w+$/.test(userId)) {
+    throw new Error(`Invalid user ID: ${userId}`)
   }
 
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  });
+  const url = await storage.get(`${userId}.url`)
+  let secret = await storage.get(`${userId}.secret`)
+  if (!secret) {
+    secret = 'SK' + require('crypto').randomBytes(20).toString('hex')
+    await storage.set(`${userId}.secret`, secret)
+  }
+
+  if (event.message.type === 'text') {
+    const text = event.message.text
+    const m1 = /^\s*\/post(?:\s+([^]+))?/i.exec(text)
+    if (text) {
+      
+    }
+  }
+
+  if (!url) {
+    return client.replyMessage(event.replyToken, [
+      {
+        type: 'text',
+        text: 'Hmmm... It seems like you haven’t set a URL yet! To set a URL, send the following command:'
+      },
+      {
+        type: 'text',
+        text: '/post set-url <URL>'
+      }
+    ]);
+    return
+  }
 }
 
 app.listen(3000);
